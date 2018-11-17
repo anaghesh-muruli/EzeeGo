8d4f331baa47c1f1660519c5da4e3b60cde2b5f1
Anaghesh Muruli <anagheshm@gmail.com>
2018-11-17 14:26:04 +0530

    initiated Scan Activity

--- app/src/main/java/anaghesh/ezeego/Shopping.java
+++ app/src/main/java/anaghesh/ezeego/Shopping.java
@@ -1,14 +1,207 @@
 package anaghesh.ezeego;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Shopping extends AppCompatActivity {
+    SurfaceView cameraPreview;
+    EditText beacon,vin;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    Button assign;
+    Camera camera = Camera.open();
+    static public  String beaconStr;
+    private TextView scanInfo;
+    final int RequestCameraPermissionID = 1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        cameraPreview =  findViewById(R.id.camera_pre);
+
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                        setCamFocusMode();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
-        //commit
+        toolbarSetup();
+        cameraPreview =  findViewById(R.id.camera_pre);
+        Log.e("error",""+cameraPreview);
+        scanInfo = findViewById(R.id.scan_info);
+        // setCamFocusMode();
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                //  .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+        //Add Event
+        //alert();
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+
+                Camera.Parameters params = camera.getParameters();
+                if (params.getSupportedFocusModes().contains(
+                        Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+                    params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+                }
+                camera.setParameters(params);
+                setCamFocusMode();
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    //Request permission
+                    ActivityCompat.requestPermissions(Shopping.this,
+                            new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                    //  setCamFocusMode();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
+                if(qrcodes.size() != 0)
+                {
+                    beacon.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            cameraSource.stop( );
+                            Log.e("QR ",""+qrcodes.valueAt(0).displayValue);
+
+                            Log.e("Beacon ",""+beacon.getText().toString());
+
+                            Log.e("VIN ",""+vin.getText().toString());
+
+                            if(beacon.getText().toString().isEmpty()) {
+                                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                                vibrator.vibrate(100);
+                                beacon.setText(qrcodes.valueAt(0).displayValue);
+                                //vinAlert();
+                                restartCamera();
+                            }
+                            else {
+                                String s = qrcodes.valueAt(0).displayValue;
+                                if(!(s.equalsIgnoreCase(beacon.getText().toString()))){
+                                    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                                    vibrator.vibrate(100);
+                                    vin.setText(s);
+                                }
+                                else{
+                                    //   Toast.makeText(ScanQR.this, "Please scan vehicle barcode", Toast.LENGTH_SHORT).show();
+                                    restartCamera();
+                                }
+                            }
+
+
+
+                        }
+                    });
+
+                }
+
+            }
+
+        });
+    }
+    void toolbarSetup(){
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+    @SuppressLint("MissingPermission")
+    public void restartCamera()
+    {
+        Log.e("method","Restart Camera");
+        try {
+            cameraSource.start(cameraPreview.getHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setCamFocusMode(){
+
+        if(null == camera) {
+            return;
+        }
+
+        /* Set Auto focus */
+        Camera.Parameters parameters = camera.getParameters();
+        List<String> focusModes = parameters.getSupportedFocusModes();
+        if(focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)){
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+        } else
+        if(focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)){
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        }
+
+        camera.setParameters(parameters);
     }
 }
