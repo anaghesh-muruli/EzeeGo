051467a85adff4e57788787eeb29678c87bf7383
Anaghesh Muruli <anagheshm@gmail.com>
2018-11-18 06:57:32 +0530

    Splash

--- app/src/main/java/anaghesh/ezeego/Login.java
+++ app/src/main/java/anaghesh/ezeego/Login.java
@@ -1,13 +1,23 @@
 package anaghesh.ezeego;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
-
+ImageView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Dashboard.class));
+            }
+        });
     }
 }
